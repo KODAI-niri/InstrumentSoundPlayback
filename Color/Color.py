eb62089e36f0f999065064d99a20c10b36fd6da9
辻 紘大 <nyx_z@tsujinoMacBook-Air.local>
2025-11-11 22:58:35 +0900

    write Color

--- Color/Color.py
+++ Color/Color.py
@@ -10,6 +10,8 @@ __author__ = "Tsuji Kodai"
 __version__ = "1.0.0"
 __date__ = "2025/10/24 (Updated: 2025/11/11)" 
 
+from Instrument.Instrument import Instrument
+
 class Color:
     """
     Colorクラス
@@ -25,3 +27,35 @@ class Color:
     Properties:
         instruments: 音を出す楽器群
     """
+
+    def __init__(self):
+        """
+        このクラスのコンストラクタ
+        instrumentsプロパティを初期化する
+        """
+
+        self.instruments = []
+    
+    def prepareToPerform(self):
+        """
+        演奏(音を出す)ための準備を行います。
+        自分が持っている楽器それぞれのmakeSoundを呼び出します
+        """
+
+        for an_instrument in self.instruments:
+            assert isinstance(an_instrument, Instrument)
+            an_instrument.makeSound()
+    
+    def perform(self):
+        """
+        演奏を行います。
+        Targets:
+            1. それぞれの楽器に対して以下を実行する
+                a. 楽器の音を得る
+                b. 全ての楽器の音に対して、以下を繰り返す
+                    b_1. 音を鳴らす(sd.play(sound))
+                    b_2. 音が鳴り終わるまで待つ
+        但し、このperformは何もしない。子クラスのperformに処理を任せる
+        """
+
+        pass
