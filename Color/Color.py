c5d2afb715a499c260c36fca87525f7215a2fefe
辻 紘大 <nyx_z@tsujinoMacBook-Air.local>
2025-11-11 22:41:59 +0900

    write Color

--- Color/Color.py
+++ Color/Color.py
@@ -8,7 +8,7 @@ Color : Blue,Green,Red,Yellowの
 
 __author__ = "Tsuji Kodai"
 __version__ = "1.0.0"
-__date__ = "2025/10/24"
+__date__ = "2025/10/24 (Updated: 2025/11/11)" 
 
 class Color:
     """
@@ -19,6 +19,9 @@ class Color:
     Target:
         0:  まずは、楽器クラスを全て作成し、正しく動作することを証明する
         1:  __init__の作成
+        2:  prepareToPerformの作成
         2:  performの作成
         3:  テスト(いらんかも)を行う
+    Properties:
+        instruments: 音を出す楽器群
     """
